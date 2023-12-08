bc73064898f39c0487c011d76730807a1fca0d34
PrabeshBimali <54845637+PrabeshBimali@users.noreply.github.com>
2023-12-08 22:37:24 +0545

    Added service page

---
+++ js/home.js
@@ -0,0 +1,36 @@
+const profileLink = `<a href="#" class="mb-profile">
+                        <img src="../img/profile2.png" />
+                        <span href="#"> Profile </span>
+                        </a>`;
+
+function toggleNavbar() {
+  const navLinks = document.querySelector(".nav-links");
+  const profile = document.querySelector(".profile")
+  navLinks.innerHTML += profileLink;
+  navLinks.classList.toggle("show")
+  profile.classList.toggle("hide")
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const testimonials = document.querySelectorAll('.testimonial');
+  let currentTestimonialIndex = 0;
+
+  function showTestimonial(index) {
+      testimonials.forEach((testimonial, i) => {
+          if (i === index) {
+              testimonial.classList.add('show-testimonial');
+          } else {
+              testimonial.classList.remove('show-testimonial');
+          }
+      });
+  }
+
+  function changeTestimonial() {
+      currentTestimonialIndex = (currentTestimonialIndex + 1) % testimonials.length;
+      showTestimonial(currentTestimonialIndex);
+  }
+
+  setInterval(changeTestimonial, 10000); // Change every 30 seconds
+  showTestimonial(currentTestimonialIndex); // Show the initial testimonial
+});
